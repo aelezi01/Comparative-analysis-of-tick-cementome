77fa8e3606915604360d54c1ca059511e575702e
aelezi01 <areda.elezi@gmail.com>
2021-04-30 20:05:33 +0100

    ae 30/04 20:05

--- VillarUNIPROTlist.py
+++ VillarUNIPROTlist.py
@@ -3,12 +3,10 @@ import pandas as pd
 # open and read the csv file
 cement_villar_file = pd.read_csv('../Data_S4.csv')
 
-# create a list with the Uniprod IDs
-uniprotIDs_list = pd.Series(list(cement_villar_file['Uniprot  accesion number']))
-uniprotIDs_list.dropna()
-print(uniprotIDs_list)
+# remove rows without Uniprod IDs
+uniprotIDs_list = cement_villar_file.dropna(subset = ['Uniprot  accesion number'])
 
-# store the list in a csv file
-#uniprotIDs_list.to_csv('Villar_uniprotIDs.csv', index=False)
+# store only the Uniprot ID values within a csv file
+uniprotIDs_list.to_csv('Villar_uniprotIDs.csv', index=False, columns = ['Uniprot  accesion number'])
 
 # the file will be used to find the corresponding gene names on Uniprot
