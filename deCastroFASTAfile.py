b07e6fe3bb6df4d124bc244ac5a44c5faaa2290e
aelezi01 <areda.elezi@gmail.com>
2021-06-02 10:23:36 +0100

    ae 2/6/21

---
+++ deCastroFASTAfile.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import requests
+import urllib.request
+
+tirloniDB = pd.read_csv('TirloniSwissprot1.csv', skiprows=[0], header=None, index_col=False)
+#tirloniDB = pd.read_csv('TirloniSwissprot2Tick.csv', skiprows=[0], header=None, index_col=False)
+#tirloniDB = pd.read_csv('TirloniSwissprot2HostContaminants.csv', skiprows=[0], header=None, index_col=False)
+print(len(tirloniDB))
+
+obsolete = []
+row = 0
+
+for i in tirloniDB.itertuples():
+    ## retrieve the sequences using Uniprot IDs from the Uniprot website
+    uniprotID = tirloniDB.iloc(axis=0)[row, 0]
+    url = 'https://www.uniprot.org/uniprot/'+ uniprotID +'.fasta'
+    row = row + 1\
+    
+    try:
+        with urllib.request.urlopen(url) as url:
+
+            ## read the content from the url and decode it
+            page = url.read()
+            seq = page.decode('utf8')
+
+            ## open and write a new fasta file with all the sequences corresponding to the Uniprot IDs stored in the csv file
+            with open('TirloniDB1.fasta', 'a') as tirloni:
+                tirloni.write(seq)
+
+            if len(seq) == 0:
+                obsolete.append(row)
+    except:
+        obsolete.append(row)
+        pass
