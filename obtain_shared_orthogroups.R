1d0afb271a287ce5b05ac531de3c2c9b9c40b54a
aelezi01 <areda.elezi@gmail.com>
2021-08-13 20:18:47 +0100

    ae

--- obtain_shared_orthogroups.R
+++ obtain_shared_orthogroups.R
@@ -12,3 +12,10 @@ library(tidyverse)
 library(here)
 
 # import dataset
+all_orthogroups <- read_tsv(here('data', 'orthofinder_results', 'MC', 'Orthogroups.tsv'))
+
+# remove empty values
+all_orthogroups <- na.omit(all_orthogroups)
+
+# save table 
+write.csv(all_orthogroups, file = here('data', 'orthofinder_results', 'MC', "shared_orthogroups.csv"), row.names = FALSE )
